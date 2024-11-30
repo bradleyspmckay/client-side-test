cbac0359f1983077d2bcf7000cf1e583fea97da9
bradleyspmckay <bradley.sp.mckay@gmail.com>
2024-11-30 15:00:23 -0600

    Implemented JQuery syntax and fixed related errors

--- assets/csrf.js
+++ assets/csrf.js
@@ -3,13 +3,15 @@ function update_action_and_method(event) {
     event.preventDefault();
 
     const form = event.target;
-    const form_to_update = $("#csrf_form");
+    const csrf_form = $("#csrf_form");
 
-    form_to_update.attr("action", form.elements["action"].value);
-    form_to_update.attr("method", form.elements["method"].value);
+    const items = ["method", "action"];
+    for (const item of items) {
+        csrf_form.attr(item, form.elements[item].value);
+    }
 
-    document.getElementById("chosen_csrf_url").innerText = form.elements["action"].value;
-    document.getElementById("chosen_csrf_method").innerText = form.elements["method"].value;
+    $("#chosen_csrf_url").text(form.elements["action"].value);
+    $("#chosen_csrf_method").text(form.elements["method"].value);
 
     form.reset();
 
@@ -21,13 +23,13 @@ function add_input(event) {
 
     const form = event.target;
     const parameter_name = form.elements["parameter_to_add"].value;
-    const form_to_change = document.getElementById("csrf_form");
-    const csrf_submit_button = document.getElementById("csrf_submit_button");
+    const form_to_change = $("#csrf_form");
+    const csrf_submit_button = $("#csrf_submit_button");
     
     const new_label = document.createElement("label");
     new_label.setAttribute("for", parameter_name);
     new_label.innerText = parameter_name + ": ";
-    form_to_change.appendChild(new_label);
+    form_to_change.append(new_label);
 
     type = check_radio_input(form);
 
@@ -36,13 +38,13 @@ function add_input(event) {
     new_input.setAttribute("name", parameter_name);
     new_input.setAttribute("id", parameter_name);
     new_input.setAttribute("class", "csrf_submission_value");
-    form_to_change.appendChild(new_input);
+    form_to_change.append(new_input);
     
-    form_to_change.appendChild(document.createElement("br"));
-    form_to_change.appendChild(document.createElement("br"));
+    form_to_change.append(document.createElement("br"));
+    form_to_change.append(document.createElement("br"));
 
-    form_to_change.removeAttribute(csrf_submit_button);
-    form_to_change.appendChild(csrf_submit_button);
+    form_to_change.remove(csrf_submit_button);
+    form_to_change.append(csrf_submit_button);
 
     form.reset();
 }
