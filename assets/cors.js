d414b2c75852303615fa02fe61743dae4be2e73e
bradleyspmckay <bradley.sp.mckay@gmail.com>
2025-01-05 12:55:08 -0600

    Commit to sync repo

--- assets/cors.js
+++ assets/cors.js
@@ -2,8 +2,15 @@ function testURL(event) {
 
     event.preventDefault();
     paragraphToUpdate = document.getElementById("results");
-    
-    fetch(event.target.elements["url_to_test"].value)
+    // TODO: Add functionality for user to decide between Simple Requests and Preflighted Requests;
+    //       Also add functionality to change the request method
+    fetch(event.target.elements["url_to_test"].value, {
+        mode: "cors",
+        method: "OPTIONS",
+        headers: {
+            "X-Arbitrary-header": "test"
+        }
+    })
     .then(response => {
         if (!response.ok) {
             throw new Error(`Network response was not ok: ${response.status}`);
