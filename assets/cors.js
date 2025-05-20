406200877e7b0882b056a2fcbad39ecfaafa7b55
BradSM <95379873+bradleyspmckay@users.noreply.github.com>
2025-05-20 15:53:46 -0500

    Update cors.js
    Updated cors.js to make fetch API more efficient with colored output in debug console.

--- assets/cors.js
+++ assets/cors.js
@@ -1,29 +1,35 @@
 function testURL(event) {
-
     event.preventDefault();
+    url = event.target.elements["url_to_test"].value
+    fetchConfig = {mode: "cors"}
     paragraphToUpdate = document.getElementById("results");
-    // TODO: Add functionality for user to decide between Simple Requests and Preflighted Requests;
-    //       Also add functionality to change the request method
-    fetch(event.target.elements["url_to_test"].value, {
-        mode: "cors",
-        method: "OPTIONS",
-        headers: {
-            "X-Arbitrary-header": "test"
-        }
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`Network response was not ok: ${response.status}`);
-        } else {
-            paragraphToUpdate.innerText = `Request successful! Status: ${response.status}\n\nThe application may be vulnerable to cross-origin attacks.`
-        }
-    })
-    .catch(error => {
-        // Handle errors, including CORS errors
-        if (error.message.includes('Failed to fetch')) {
-            paragraphToUpdate.innerText = 'CORS error: The request was blocked by CORS policy.';
-        } else {
-            paragraphToUpdate.innerText =  `An error occurred: ${error.message}`;
-        }
-    });
-    }
+    consoleStyle = "color: #f96d00; background: #393e46; font-weight: bold"
+
+    fetch(url, fetchConfig)
+        // Make request and log response code, continue if successful
+        .then(response => {
+            if (!response.ok) {
+                paragraphToUpdate.innerText = `Network response not ok: ${response.status}\n\nCheck debug console for further information.`
+            } else {
+                paragraphToUpdate.innerText = `Request successful! Status: ${response.status}\n\nThe application may be vulnerable to cross-origin attacks.\n\nCheck debug console for captured data`
+                console.log("%c  -- RESPONSE HEADERS --  ", consoleStyle)
+                headerString = ""
+                for (const [key, value] of response.headers.entries()) {
+                    headerString += `${key}: ${value}\n`
+                }
+                console.log(headerString)
+                return response
+            }
+        })
+        // Convert response body into text
+        .then(response => response.text())
+        // Log body data
+        .then(body => {
+            console.log("%c  -- RESPONSE BODY --  ", consoleStyle)
+            console.log(body.replace(/[\r\n\r\n]+/, ""))
+        })
+        // Error handling
+        .catch(error => {
+            console.error(error)
+        })
+}
