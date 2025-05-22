cdd3a50740342f22bbdee2d69c0fc325ec4f6e2c
bradleyspmckay <bradley.sp.mckay@gmail.com>
2025-05-22 16:37:50 -0500

    New favicon and updated CORS output text

--- assets/cors.js
+++ assets/cors.js
@@ -9,9 +9,9 @@ function testURL(event) {
         // Make request and log response code, continue if successful
         .then(response => {
             if (!response.ok) {
-                paragraphToUpdate.innerText = `Network response not ok: ${response.status}\n\nCheck debug console for further information.`
+                paragraphToUpdate.innerText = `Network response not ok: ${response.status}\n\nCheck the developer console for further details.`
             } else {
-                paragraphToUpdate.innerText = `Request successful! Status: ${response.status}\n\nThe application may be vulnerable to cross-origin attacks.\n\nCheck debug console for captured data`
+                paragraphToUpdate.innerText = `Request successful! Status: ${response.status}\n\nThe application may be vulnerable to cross-origin attacks.\n\nCheck developer console for response details`
                 console.log("%c  -- RESPONSE HEADERS --  ", consoleStyle)
                 headerString = ""
                 for (const [key, value] of response.headers.entries()) {
